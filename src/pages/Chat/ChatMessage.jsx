d2d8086ac3853cb832231c9535fa28316d17f61d
yutzu <tsengm6h6@gmail.com>
2023-01-18 17:39:26 +0800

    modify message ui

--- src/pages/Chat/ChatMessage.jsx
+++ src/pages/Chat/ChatMessage.jsx
@@ -16,13 +16,17 @@ const ChatMessage = forwardRef(
       }
     }, [])
 
-    const userId = [3]
+    const userId = [3, 5, 6]
     const fromSelf = userId.includes(id)
   
     return (
       <Message className={fromSelf ? 'self' : null } ref={messageRef}>
         <Avatar size="medium" src={avatar} />
         <Text className={fromSelf ? 'self' : null }>{text}</Text>
+        <MessageDetail>
+          <Status>read</Status>
+          <Time>{time}</Time>
+        </MessageDetail>
       </Message>
     )
   }
@@ -31,8 +35,8 @@ const ChatMessage = forwardRef(
 const Message = styled.div `
   display: flex;
   align-items: flex-start;
-  gap: 1rem;
-  margin: 0.5rem 0;
+  gap: 0.5rem;
+  margin: 1.5rem 0;
 
   &.self {
     flex-direction: row-reverse;
@@ -42,6 +46,7 @@ const Message = styled.div `
 
 const Text = styled.p `
   padding: 1rem 1rem;
+  margin-left: 0.5rem;
   background-color: var(--bg-color-darken);
   border-radius: 20px;
   border-top-left-radius: 4px;
@@ -50,8 +55,26 @@ const Text = styled.p `
   &.self {
     border-top-right-radius: 4px;
     border-top-left-radius: 20px;
+    background-color: var(--secondary);
+    color: ${props => props.theme.mode === 'light' ? 'var(--bg-color-main)' : 'var(--main-color)'};
   }
 `
 
+const MessageDetail = styled.div `
+  align-self: flex-end;
+  color: var(--main-color);
+`
+
+const Status = styled.span `
+  font-size: 0.75rem;
+  text-transform: capitalize;
+  font-weight: 400;
+`
+
+const Time = styled.p `
+  font-size: 0.75rem;
+  font-weight: 600;
+  margin-bottom: 4px;
+`
 
 export default ChatMessage
